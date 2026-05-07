7d0dcc14850dfb8452e09607ff31d48459880640
Ludvig Sandh <givdul11@hotmail.se>
2026-05-07 14:32:14 +0200

    feat: update scalability benchmark for athena

--- tools/benchmark_scalability.py
+++ tools/benchmark_scalability.py
@@ -5,25 +5,24 @@ from benchmarking import BenchmarkGroup, Params, parse_instance_arg, run_benchma
 # Omitted values resolve to 1 after benchmark parameters are combined.
 GROUP = BenchmarkGroup(
     name="scalability",
-    timeout=60*10,
+    timeout=60*30,
     params_fallback=Params(
         problem="max_clique",
         batch=None,
-        stickiness=16,
         num_repetitions=5,
     ),
     params_x=[
         Params(pq_type="seq_stack", name="Sequential Stack", threads=1),
-        Params(pq_type="locked_stack", name="Globally locked Stack"),
+        Params(pq_type="locked_stack", name="Globally locked Stack", batch=16),
         Params(pq_type="pmc", name="PMC library"),
-        Params(pq_type="work_stealing", name="Simple work stealing"),
-        Params(pq_type="multilifo", name="MultiLIFO", num_repetitions=20),
-        Params(pq_type="treiber_stack", name="Treiber stack (elimination)"),
-        Params(pq_type="2d_stack", name="2D stack"),
+        Params(pq_type="work_stealing", name="Simple work stealing", batch=16),
+        Params(pq_type="multilifo", name="MultiLIFO", num_repetitions=20, batch=16),
+        Params(pq_type="treiber_stack", name="Treiber stack (elimination)", batch=16),
+        Params(pq_type="2d_stack", name="2D stack", batch=16),
         Params(pq_type="seq_pq", name="Sequential PQ", threads=1),
-        Params(pq_type="locked_pq", name="Globally locked PQ"),
-        Params(pq_type="pr", name="PR (batch=16)", batch=16),
-        Params(pq_type="mq_stick_swap", name="MultiQueue (stick swap, batch=16)", batch=16),
+        Params(pq_type="locked_pq", name="Globally locked PQ", batch=16),
+        Params(pq_type="pr", name="PR", batch=16),
+        Params(pq_type="mq_stick_swap", name="MultiQueue (stick swap)", batch=16),
     ],
     params_y=[
         Params(threads=1),
@@ -31,7 +30,11 @@ GROUP = BenchmarkGroup(
         Params(threads=4),
         Params(threads=8),
         Params(threads=16),
-        Params(threads=24),
+        Params(threads=32),
+        Params(threads=64),
+        Params(threads=128),
+        Params(threads=256),
+        Params(threads=512)
     ],
 )
 
