ee6424f8d906c4c574fc0695c1ce5774b8eff1b5
Ludvig Sandh <givdul11@hotmail.se>
2026-05-29 11:31:19 +0200

    feat: Fix names of solvers generated in benchmark script results

--- tools/benchmark_batch.py
+++ tools/benchmark_batch.py
@@ -15,10 +15,10 @@ GROUP = BenchmarkGroup(
         Params(pq_type="locked_stack", name="Globally locked Stack"),
         Params(pq_type="work_stealing", name="Simple work stealing"),
         Params(pq_type="multilifo", name="MultiLIFO"),
-        Params(pq_type="treiber_stack", name="Treiber stack (elimination)"),
-        Params(pq_type="2d_stack", name="2D stack"),
+        Params(pq_type="treiber_stack", name="Treiber stack"),
+        Params(pq_type="2d_stack", name="2D-stack"),
         Params(pq_type="locked_pq", name="Globally locked PQ"),
-        Params(pq_type="pr", name="PR"),
+        Params(pq_type="pr", name="Linden"),
         Params(pq_type="mq_stick_swap", name="MultiQueue (stick swap)"),
     ],
     params_y=[
